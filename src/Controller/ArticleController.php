b5ecb35a4e137e26199b599e8ceba051d389b9a1
lngcw <lngcw@mail.ru>
2018-07-03 16:42:03 +0300

    markdown service markdown cache mardown helpers

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
+use App\Service\MarkdownHelper;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends AbstractController
 {
@@ -12,8 +13,10 @@ class ArticleController extends AbstractController
         return $this->render("article/homepage.html.twig");
     }
 
-    public  function news($id)
+    public  function news($id, LoggerInterface $logger)
     {
+        $logger->info("Was request news page");
+
         return $this->render("article/show.html.twig", [
             'id' => $id
         ]);
@@ -24,5 +27,21 @@ class ArticleController extends AbstractController
         $num = mt_rand(1, 10);
 
         return $this->json($num);
+    }
+
+    public function mark(MarkdownHelper $markdownHelper)
+    {
+        $mark = <<<EOF
+HERE **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+turkey shank eu pork belly meatball non cupim.
+EOF;
+
+        $mark = $markdownHelper->parse($mark);
+
+        return $this->render('article/mark.html.twig', [
+            'mark' => $mark
+        ]);
     }
 }
